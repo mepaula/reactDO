a6054bfb6fcd782a68a09eeb1a3a393d1a04bedd
mepaula <melinad.pontes@gmail.com>
2023-08-30 09:22:18 -0300

    Status funcionando

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 function App() {
 
   const [ listaTarefas, setListaTarefas] = useState( [] );
-  const [ tarefa, setTarefa ] = useState( { id: '' , texto: "" } );
+  const [ tarefa, setTarefa ] = useState( { id: '' , texto: "", status: ""} );
 
   function addTarefa()
   {
@@ -14,12 +14,19 @@ function App() {
 
   function excluirTarefa( id )
   {
-     const novaLista =  listaTarefas.filter( tarefa => tarefa.id !== id );
+     const novaLista =  listaTarefas.filter( ( tarefa ) => tarefa.id !== id );
      setListaTarefas( novaLista );
   }
 
+  function statusTarefa( id, status)
+  {
+      const index = listaTarefas.findIndex( ( tarefa ) => tarefa.id === id );
+      listaTarefas[index].status = !status;
+      setListaTarefas( [...listaTarefas] );
+  }
+
   useEffect( () => {
-      setTarefa( { id: "" , texto: "" } );
+      setTarefa( { id: "" , texto: "", status: "" } );
   }, [ listaTarefas ] )
 
   return (
@@ -28,13 +35,13 @@ function App() {
           <h1>Lista de Tarefas para Fazer:</h1>
         </header>
         <div>
-          <input type="text" name="tarefa" placeholder="Escreva a sua tarefa" value={tarefa.texto} onChange={ (e) => setTarefa( {id: Math.random(), texto: e.target.value} ) }/>
+          <input type="text" name="tarefa" placeholder="Escreva a sua tarefa" value={tarefa.texto} onChange={ (e) => setTarefa( {id: Math.random(), texto: e.target.value, status: false} ) }/>
           <button onClick={addTarefa}>Ver tarefas</button>
         </div>
         <div>
           <ul>
             {listaTarefas.map( (item, index) => (
-              <li key={item.id}>{item.texto} <button onClick={ () => excluirTarefa(item.id) }>Excluir</button></li>
+              <li key={item.id}>{item.texto}<button onClick={ () => statusTarefa(item.id, item.status) }>{item.status ? 'Concluida' : 'Não Concluida' }</button> <button onClick={ () => excluirTarefa(item.id) }>Excluir</button></li>
             ))}
           </ul>
         </div>
